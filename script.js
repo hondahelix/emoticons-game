e71cffbfd6120efb8b7a768d5e803c6b1000a79e
Jonathan Honda <hondahelix120@gmail.com>
2020-12-15 20:25:17 -0800

    added attack and have it to show on spacebar click

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var character = document.getElementById("character");
+var attack = document.getElementById("attack");
 var interval;
 var both = 0;
+var facing  = "right";
 // function characterJump(){
 //     if(character.classList !== "animate"){
 //         character.classList.add("animate");
@@ -19,6 +21,9 @@ document.addEventListener('keydown', event => {
         setTimeout(()=>{character.classList.remove("jump")},500);
 
     }
+    if((event.code === 'Space')){
+        handleAttack()
+    }
     //catches to make sure both left and right not being pressed
     if(both ===0){
         both++;
@@ -33,12 +38,30 @@ document.addEventListener('keydown', event => {
 
 function moveLeft(){
     var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    character.style.left = left - 2+"px";
+    if(left>0){
+        character.style.left = left - 2+"px";
+        facing  = "left";
+    }
 }
 
 function moveRight(){
     var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    character.style.left = left + 2+"px";
+    if(left<540){
+        character.style.left = left + 2+"px";
+        facing  = "right";
+    }
+    
+}
+
+function handleAttack(){
+    
+    // var startAttackLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    // var startAttackTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    // attack.style.left = startAttackLeft + 15+ "px";
+    // attack.style.top = startAttackTop + "px";
+    //need to get this to move accross the board"
+    attack.classList.remove("hidden");
+
 }
 
 document.addEventListener('keyup', event => {
